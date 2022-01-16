b61328d881a0a7b1db2743a2d7e48a9421183a28
Diego Niquefa <diego@valiu.co>
2022-01-16 18:42:56 -0500

    add model unit tests for album

--- test/models/album_test.rb
+++ test/models/album_test.rb
@@ -1,7 +1,31 @@
 require 'test_helper'
 
 class AlbumTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  valid_album_data = {
+    name: "Super hit album",
+    image_url: "https://iamges.spotify.com/dope-album-cover.jpeg",
+    spotify_url: "https://albums.spotify.com/super-hit-album.html",
+    spotify_id: "s9dabm324",
+    total_tracks: 9
+  }
+
+  test "should not save album without a name" do
+    album = Album.new valid_album_data.except :name
+    assert_not album.save
+  end
+
+  test "should not save album without a spotify_url" do
+    album = Album.new valid_album_data.except :spotify_url
+    assert_not album.save
+  end
+
+  test "should not save album without a spotify_id" do
+    album = Album.new valid_album_data.except :spotify_id
+    assert_not album.save
+  end
+
+  test "should create an album with valid fields" do
+    album = Album.new valid_album_data
+    assert album.save
+  end
 end
