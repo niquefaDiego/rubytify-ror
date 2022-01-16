6ac700460f2d9f970c9d287c4a390f9fa0258233
Diego Niquefa <diego@valiu.co>
2022-01-16 15:37:04 -0500

    add GET /api/v1/artists/1/albums endpoint

--- app/controllers/api/v1/artists_controller.rb
+++ app/controllers/api/v1/artists_controller.rb
@@ -16,6 +16,18 @@ module Api
       end
 
       def list_albums
+        artist = Artist.find(params[:id])
+        all_artist_albums = artist.albums.all.select(:id, :name, :image_url, :spotify_url, :total_tracks)
+        all_artist_albums = all_artist_albums.map do |album|
+          {
+            id: album.id,
+            name: album.name,
+            image: album.image_url,
+            spotify_url: album.spotify_url,
+            total_tracks: album.total_tracks
+          }
+        end
+        render json: all_artist_albums
       end
     end
   end
