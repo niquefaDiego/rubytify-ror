01f47c56338a1c3093b0a49ecd29a03e6298f5ba
Diego Niquefa <diego@valiu.co>
2022-01-16 15:50:25 -0500

    add GET /api/v1/albums/:id/songs endpoint

--- app/controllers/api/v1/albums_controller.rb
+++ app/controllers/api/v1/albums_controller.rb
@@ -3,6 +3,8 @@ module Api
     class AlbumsController < ApplicationController
       def list_songs
         album = Album.find_by spotify_id: params[:id]
+        songs = album.songs.all.select(:name, :spotify_url, :preview_url, :duration_ms, :explicit)
+        render json: songs.as_json(:except => :id)
       end
     end
   end
